47644471ca7d395191d2522e6f398246ebc46408
ZakurOwO <johndanielnavarro130@gmail.com>
2025-09-16 15:26:06 +0800

    Added the code in number 4

--- Manually Throwing Custom Exception/Form1.Designer.cs
+++ Manually Throwing Custom Exception/Form1.Designer.cs
@@ -214,6 +214,7 @@
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.gridViewProductList)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
